d2e78dbd8e61092f7b385bbf9366f60682f459a8
marvslopez <marvinmlopez15@gmail.com>
2024-06-01 10:48:58 +0800

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,11 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>TangMasarappp</h1>
+            <h1>TangNyaman</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item active">TangMasarap</li>
+              <li class="breadcrumb-item active">TangNyaman</li>
             </ol>
           </div>
         </div>
